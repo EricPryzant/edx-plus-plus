e3dfa1076f4e99d9cac9fe928658449bc4d1c239
Rick Gray <Rick.Gray@INEIGHT.COM>
2021-08-27 13:44:55 -0500

    fix multi video, remove deprec code, cleanup

--- Userscript/edX-plus-plus.user.js
+++ Userscript/edX-plus-plus.user.js
@@ -10,43 +10,51 @@
 // @run-at document-idle
 // ==/UserScript==
 
-(function() {
+(() => {
     'use strict';
-    var vid = document.getElementsByClassName("video")[0];
-    var vid_url = vid.attributes['data-metadata'].value.toString().match("https://edx-video.net/[\\w\-\.]+.mp4");
-    var vid_title = vid.previousElementSibling.innerText;
-    var zNode = document.createElement ('div');
-    
-    if(vid_url.length === 1) {
-        if(vid_title.toLowerCase() === "video"){
-            vid_title = "Lecture Video"
-        } else {
-            vid_title += " Lecture Video"
+    const vids = document.getElementsByClassName("video");
+    const vid_urls = [];
+    const vid_titles = [];
+    for (const vid of vids) {
+        const url = vid.attributes['data-metadata'].value.match("https://edx-video.net/[\\w\-\.]+.mp4");
+        const title = vid.previousElementSibling.innerText;
+        if (url) {
+            vid_urls.push(url);
+            vid_titles.push(title);
         }
-        zNode.innerHTML = '<a href=' + vid_url[0] + '><button id="myButton" type="button">Download ' + vid_title + '</button></a>';
+    }
+
+    const zNode = document.createElement('div');
+
+    if(vid_urls.length === 1) {
+        if(vid_titles[0].toLowerCase() === "video"){
+            vid_titles[0] = "Lecture Video"
+        } else {
+            vid_titles[0] += " Lecture Video"
+        }
+        zNode.innerHTML = '<a href=' + vid_urls[0] + '><button type="button">Download ' + vid_titles[0] + '</button></a>';
     } else {
-        var buttonText = ""
-        vid_url.forEach(function(item, idx) {
-            buttonText += '<a href=' + item + '><button id="myButton" type="button" style="margin: 5px; min-width: 250px">Download Video ' + (idx+1) + '</button></a>'
-        })
-        zNode.innerHTML = '<button id="myButton" type="button">Download Lecture Videos</button>'
-        zNode.onclick = function () {
+        let buttonText = ""
+        vid_urls.forEach((item, idx) => {
+            buttonText += '<a href=' + item + '><button type="button" style="margin: 5px; min-width: 250px">Download Video ' + (idx+1) + '</button></a>'
+        });
+        zNode.innerHTML = '<button type="button">Download Lecture Videos</button>'
+        zNode.onclick = () => {
             swal.fire({
                 title: "Multiple Videos Detected",
                 icon: "info",
                 html: buttonText
-            })
-        }
+            });
+        };
     }
     
-    zNode.setAttribute ('id', 'myContainer');
-    zNode.style = "top:0;right:0;position:absolute;z-index:99999;padding:20px;";
-    document.body.appendChild (zNode);
+    zNode.style = "top:0;right:7px;position:absolute;z-index:99999;padding:20px;";
+    document.body.appendChild(zNode);
 
-    var item = document.getElementsByClassName("video-speeds")[0];
-    var new_speed = document.createElement("li");
-    var btn = document.createElement("button");
-    var speed_limit = "2.50";
+    let item = document.getElementsByClassName("video-speeds")[0];
+    let new_speed = document.createElement("li");
+    let btn = document.createElement("button");
+    let speed_limit = "2.50";
     btn.setAttribute("class", "control speed-option");
     btn.setAttribute("tabindex", -1);
     btn.setAttribute("aria-pressed", "false");
@@ -79,34 +87,33 @@
     new_speed.children[0].innerText = speed_limit + "x";
     item.prepend(new_speed);
 
-    const keySpace = 32;
-    const keyLeft = 37;
-    const keyRight = 39;
-    document.addEventListener("keydown", function(event) {
-        const code = event.keyCode;
+    const keySpace = 'Space';
+    const keyLeft = 'ArrowLeft';
+    const keyRight = 'ArrowRight';
+    document.addEventListener("keydown", (event) => {
+        const code = event.code;
         const alt = event.altKey;
         //console.log("Key pressed " + code + " " + alt);
         if (code == keySpace) {
-            var playPauseButton = document.getElementsByClassName("video_control");
-            playPauseButton[0].click()
+            const playPauseButton = document.getElementsByClassName("video_control");
+            playPauseButton[0].click();
             event.preventDefault();
         } else if (code == keyLeft) {
             if (!alt) {
-                document.getElementsByClassName("current")[0].previousElementSibling.children[0].click()
+                document.getElementsByClassName("current")[0].previousElementSibling.children[0].click();
                 event.preventDefault();
             } else {
-                document.getElementsByClassName("current")[0].previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.children[0].click()
+                document.getElementsByClassName("current")[0].previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.children[0].click();
                 event.preventDefault();
             }
         } else if (code == keyRight) {
             if (!alt) {
-                document.getElementsByClassName("current")[0].nextElementSibling.children[0].click()
+                document.getElementsByClassName("current")[0].nextElementSibling.children[0].click();
                 event.preventDefault();
             } else {
-                document.getElementsByClassName("current")[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.children[0].click()
+                document.getElementsByClassName("current")[0].nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.children[0].click();
                 event.preventDefault();
             }
         }
-
-    })
+    });
 })();
